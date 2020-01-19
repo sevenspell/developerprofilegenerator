574bcb6f6a5e0a7101f11a105e86456c986162e2
Seven <sevenspell@gmail.com>
2020-01-19 22:22:58 +1100

    npm installed inquirer and added functions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,55 @@
+const generateHTML = require("./generateHTML");
+const convertHTMLToPDF = require("pdf-puppeteer");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
 const questions = [
-  
+    {
+        type: "input",
+        message: "Enter your Github username:",
+        name: "usernameV"
+    }
 ];
 
-function writeToFile(fileName, data) {
- 
+
+inquirer.prompt(questions).then(function ({ usernameV }) {
+
+    const queryURLRepo = `https://api.github.com/users/${usernameV}/repos?per_page=100`;
+    const queryURLNumPublicRepo = `https://api.github.com/users/${usernameV}/repos?per_page=100`;
+    const queryURLNumFollowers = `https://api.github.com/users/${usernameV}**?per_page=100`;
+    const queryURLNumGithubStars = `https://api.github.com/users/${usernameV}**?per_page=100`;
+    const queryURLNumFollowing = `https://api.github.com/users/${usernameV}**?per_page=100`;
+    const queryURLBioImage = `https://api.github.com/users/${usernameV}/**?per_page=100`;
+
+
+    const queryURLUserLocation = `https://api.github.com/users/${usernameV}/**?per_page=100`;
+    const queryURLGithubProfile = `https://api.github.com/users/${usernameV}/**?per_page=100`
+
+
+
+});
+
+function getRepo(queryURLRepo){
+
+    axios.get(queryURLRepo).then(function (res) {
+        const repoNames = res.data.map(function (repo) {
+            return repo.name;
+            
+        });
+        console.log(repoNames);
+    })
 }
 
-function init() {
 
-init();
+
+
+
+// function writeToFile(fileName, data) {
+
+// }
+
+// function init() {
+
+// }
+
+// init();
