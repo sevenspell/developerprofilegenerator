a74d022348cc1ed6a9f9184a8c803ac55f2730d6
Seven <sevenspell@gmail.com>
2020-01-20 01:32:19 +1100

    completed github data pulling function

--- Develop/index.js
+++ Develop/index.js
@@ -3,42 +3,107 @@ const convertHTMLToPDF = require("pdf-puppeteer");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
+//1. Prompt username
 const questions = [
     {
         type: "input",
         message: "Enter your Github username:",
         name: "usernameV"
+    },
+    {
+        type: "list",
+        message: "Choose your favourite color:",
+        name: "colorV",
+        choices: ["Green", "Purple", "Pink", "Red", "Blue", "Yellow"]
     }
+
 ];
 
+const userGithubData = [];
+const starredURL = [];
 
-inquirer.prompt(questions).then(function ({ usernameV }) {
+//2. get data from Github based on username profile
+inquirer.prompt(questions[0])
 
-    const queryURLRepo = `https://api.github.com/users/${usernameV}/repos?per_page=100`;
-    const queryURLNumPublicRepo = `https://api.github.com/users/${usernameV}/repos?per_page=100`;
-    const queryURLNumFollowers = `https://api.github.com/users/${usernameV}**?per_page=100`;
-    const queryURLNumGithubStars = `https://api.github.com/users/${usernameV}**?per_page=100`;
-    const queryURLNumFollowing = `https://api.github.com/users/${usernameV}**?per_page=100`;
-    const queryURLBioImage = `https://api.github.com/users/${usernameV}/**?per_page=100`;
+    .then(function getUserData({ usernameV }) {
+
+        const queryURLGithubProfile = `https://api.github.com/users/${usernameV}`;
+        const queryURLGithubStarred = `https://api.github.com/users/${usernameV}/starred`;
+
+        axios.get(queryURLGithubProfile).then(function (res) {
+            const responseData = res.data;
+
+            //3. store username profile data into array
+            userGithubData.push(responseData);
+
+            //4. create variables to store
+            //4a. number of public repos
+            const dataNumPublicRepo = userGithubData[0].public_repos;
+            console.log(dataNumPublicRepo);
+
+            //4b. number of followers
+            const dataNumFollowers = userGithubData[0].followers;
+            console.log(dataNumFollowers);
+
+            //4d. number of followings
+            const dataNumFollowing = userGithubData[0].following;
+            console.log(dataNumFollowing);
+
+            //4e. user location
+            const dataUserLocation = userGithubData[0].location;
+            console.log(dataUserLocation);
+
+            //4f. user bio image
+            const dataUserImage = userGithubData[0].avatar_url;
+            console.log(dataUserImage);
+
+            //4g. user's github link
+            const dataUserGithub = userGithubData[0].html_url;
+            console.log(dataUserGithub);
 
 
-    const queryURLUserLocation = `https://api.github.com/users/${usernameV}/**?per_page=100`;
-    const queryURLGithubProfile = `https://api.github.com/users/${usernameV}/**?per_page=100`
-
-
-
-});
-
-function getRepo(queryURLRepo){
-
-    axios.get(queryURLRepo).then(function (res) {
-        const repoNames = res.data.map(function (repo) {
-            return repo.name;
-            
+        }).catch(function (error) {
+            console.log(error);
         });
-        console.log(repoNames);
+
+        axios.get(queryURLGithubStarred).then(function (res) {
+            const responseStarred = res.data;
+
+            responseStarred.forEach(function (responseStarred) {
+                starredURL.push(responseStarred.html_url);
+            })
+            console.log(starredURL);
+
+            //4c. number of github stars
+            const numOfStarredURL = starredURL.length
+            console.log(numOfStarredURL);
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+
     })
-}
+
+    // inquirer.prompt(questions[1])
+    // .then(function getFavColor({ colorV }){
+    //     const favColor = `${colorV}`;
+    //     console.log(favColor);
+    
+    
+    // }).catch(function (error) {
+    //     console.log(error);
+    // })
+
+
+
+//5. feed data into generateHTML() 
+
+// const htmlString = generateHTML();
+
+//6. generate html
+//7. convert html file to pdf
+
+
 
 
 
